0dce448ffebe5f86b48e3b10fce26320db2670b5
Ahmad M.Sayed <ahmed.sayed.fcis1997@gmail.com>
2021-05-23 21:38:19 +0200

    file structure

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+router.use("/users", require("./api/users"));
+router.use("/auth", require("./api/auth"));
+router.use("/posts", require("./api/posts"));
+router.use("/comments", require("./api/comments"));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ require("dotenv").config();
 connectDB();
 app.get("/", (req, res) => res.send("API Running"));
 
+app.use("/api", require("./routes"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is started on port ${PORT}`);
